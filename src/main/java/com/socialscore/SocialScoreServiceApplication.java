91f07e8e9d494c87947827c5b509911d3b64ee4c
Aleksei Novikov <novikov.aleksei.92@gmail.com>
2020-08-05 13:37:12 +0300

    Init commit

---
+++ src/main/java/com/socialscore/SocialScoreServiceApplication.java
@@ -0,0 +1,13 @@
+package com.socialscore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocialScoreServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SocialScoreServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/socialscore/SocialScoreServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.socialscore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocialScoreServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
