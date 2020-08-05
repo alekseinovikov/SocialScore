3eb6f7597235d15948aee1ac5eda379c264df5de
Aleksei Novikov <novikov.aleksei.92@gmail.com>
2020-08-05 15:16:57 +0300

    Created project's structure with modules and their dependencies

---
+++ consumer/src/main/java/com/socialscore/consumer/SocialScoreConsumerApplication.java
@@ -0,0 +1,13 @@
+package com.socialscore.consumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocialScoreConsumerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SocialScoreConsumerApplication.class, args);
+    }
+
+}
--- provider/src/main/java/com/socialscore/provider/SocialScoreProviderApplication.java
+++ provider/src/main/java/com/socialscore/provider/SocialScoreProviderApplication.java
@@ -1,13 +1,13 @@
-package com.socialscore;
+package com.socialscore.provider;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SocialScoreServiceApplication {
+public class SocialScoreProviderApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(SocialScoreServiceApplication.class, args);
+        SpringApplication.run(SocialScoreProviderApplication.class, args);
     }
 
 }
